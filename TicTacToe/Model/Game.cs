5453a45ad9c670d2faf1a3498c6b2bd36940de3a
noemimorales <79290160+noemimorales@users.noreply.github.com>
2021-08-30 21:59:47 -0300

    start test

---
+++ TicTacToe/Model/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    internal class Game
+    {
+        private string currentToken;
+        private string[,] matrixWinner = new string[,] { 
+            { "X", "X","X" },
+            { "", "",""},
+            { "", "", "" } };
+
+        private string[,] matrix = new string[,] {
+            { "", "","" },
+            { "O", "O",""},
+            { "", "", "" } };
+        public void Play(int x, int y,string token)
+        {
+            currentToken = token;
+            matrix[x, y] = currentToken;
+        }
+        
+        public bool IsGameOver()
+        {
+            var pos1 = matrix.GetValue(0, 0).ToString();
+            var pos2 = matrix.GetValue(0, 1).ToString();
+            var pos3 = matrix.GetValue(0, 2).ToString();
+            var pos4 = matrix.GetValue(1, 0).ToString();
+            var pos5 = matrix.GetValue(1, 1).ToString();
+            var pos6 = matrix.GetValue(1, 2).ToString();
+            var pos7 = matrix.GetValue(2, 0).ToString();
+            var pos8 = matrix.GetValue(2, 1).ToString();
+            var pos9 = matrix.GetValue(2, 2).ToString();
+
+            return (String.Compare(pos1,pos2)==0)&& (String.Compare(pos1, pos3) == 0);
+        }
+
+        public string GetCurrentPlayer()
+        {
+            return currentToken == "X" ? "O" : "X";
+        }
+    }
+}
---
+++ TicTacToe/Tests/GameShould.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TicTacToe
+{
+    [TestFixture]
+    public class GameShould
+    {
+        Game game;
+        [SetUp]
+        public void Before()
+        {
+            game = new Game();
+        }
+        [Test]
+        public void GameOverWhenARowIsComplete()
+        {
+            //given
+
+            //when
+            game.Play(0, 0,"X");
+            game.Play(0, 1,"X");
+            game.Play(0, 2,"X");
+
+            //then
+            Assert.True(game.IsGameOver());
+        }
+
+        [TestCase("O","X")]
+        [TestCase("X", "O")]
+        public void PassTurnAfterPlayO(string currentPlayer, string nextPlayer)
+        {
+            //given
+
+            //when
+            game.Play(0, 0, currentPlayer);
+            //then
+            Assert.AreEqual(nextPlayer, game.GetCurrentPlayer());
+        }
+    }
+}
